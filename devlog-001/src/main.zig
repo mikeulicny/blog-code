5d06ffb28e99728b56d0d7c2c57ce37fc08ff789
Mike Ulicny <MichaelAUlicny@gmail.com>
2025-08-04 22:32:29 -0400

    Got it working correctly this time

--- devlog-001/src/main.zig
+++ devlog-001/src/main.zig
@@ -3,7 +3,8 @@ const assert = std.debug.assert;
 const Allocator = std.mem.Allocator;
 var prng = std.Random.DefaultPrng.init(42);
 
-const alpha: f64 = 0.5;
+const alpha: f64 = 0.0001;
+//const lambda: f64 = 10;
 
 // Input is 784 x 1
 // vectorized: 784 x 10,000
@@ -11,7 +12,7 @@ const alpha: f64 = 0.5;
 fn random_vector(comptime m: usize) [m]f64 {
   var output: [m]f64 = undefined;
   for (output, 0..) |_, idx| {
-    output[idx] = prng.random().float(f64) * 2 - 1;
+    output[idx] = prng.random().float(f64) * 0.05;
   }
   return output;
 }
@@ -20,7 +21,7 @@ fn random_matrix(comptime m: usize, comptime n: usize) [m][n]f64 {
   var output: [m][n]f64 = undefined;
   for (output, 0..) |row, row_idx| {
     for (row, 0..) |_, col_idx| {
-      output[row_idx][col_idx] = (prng.random().float(f64) * 2 - 1) * 0.1;
+      output[row_idx][col_idx] = prng.random().float(f64) * 0.05;
     }
   }
   return output;
@@ -34,14 +35,14 @@ fn one_hot(label: u8) [10]f64 {
 
 pub fn ReLU(comptime T: type, comptime m: usize, x: [m]T) [m]T {
   var output: [m]T = undefined;
-  for (x, 0..) |row, idx| {
-    output[idx] = @max(0, row);
+  for (&output, x) |*out, in| {
+    out.* = @max(0, in);
   }
   return output;
 }
 
 pub fn ReLU_deriv(comptime T: type, x: T) T {
-  return if (x < 0) @as(T, 0) else @as(T, 1);
+  return if (x < 0) 0.0 else 1.0;
 }
 
 // Softmax
@@ -58,9 +59,9 @@ pub fn softmax(comptime T: type, comptime m: usize, v: [m]T) [m]T{
   }
 
   // Find exponentials relative to max value (this prevents overflow)
-  for (v, 0..) |val, idx| {
-    output[idx] = @exp(val - max);
-    sum += output[idx];
+  for (&output, v) |*out, val| {
+    out.* = @exp(val - max);
+    sum += out.*;
   }
 
   // Normalize
@@ -73,26 +74,26 @@ pub fn softmax(comptime T: type, comptime m: usize, v: [m]T) [m]T{
 
 pub fn add(comptime T: type, comptime m: usize, v1: [m]T, v2: [m]T) [m]T {
   var output: [m]T = undefined;
-  for (v1, v2, 0..) |x, y, idx| {
-    output[idx] = x + y;
+  for (&output, v1, v2) |*out, x, y| {
+    out.* = x + y;
   }
   return output;
 }
 
 pub fn sub(comptime T: type, comptime m: usize, v1: [m]T, v2: [m]T) [m]T {
   var output: [m]T = undefined;
-  for (v1, v2, 0..) |x, y, idx| {
-    output[idx] = x - y;
+  for (&output, v1, v2) |*val, x, y| {
+     val.* = x - y;
   }
   return output;
 }
 
 pub fn vsum(comptime T: type, comptime m: usize, v: [m]T) T {
-  var output: f64 = 0.0;
+  var sum: f64 = 0.0;
   for (v) |val| {
-    output += val;
+    sum+= val;
   }
-  return output;
+  return sum;
 }
 
 // Dot Product
@@ -110,7 +111,7 @@ pub fn dot(comptime T: type, comptime m: usize, comptime n: usize, mat1: [m][n]T
 
 pub fn vmax(comptime T: type, v: [10]T) usize {
   var output: usize = 0;
-  var max: T = @as(T, 0.0);
+  var max: f64 = 0.0;
   for (v, 0..) |i, idx| {
     if (i > max) {
       max = i;
@@ -159,30 +160,32 @@ pub fn main() !void {
   // weights: 256 x 784
   // neurons: 256
   // bias: 256
-  var w1: [256][784]f64 = random_matrix(256, 784);
-  var b1: [256]f64 = random_vector(256);
+  var w1: [20][784]f64 = random_matrix(20, 784);
+  var b1: [20]f64 = random_vector(20);
 
   // Hidden layer 2 (output layer)
   // weights: 10 x 256
   // neurons: 10
   // bias: 10
-  var w2: [10][256]f64 = random_matrix(10, 256);
+  var w2: [10][20]f64 = random_matrix(10, 20);
   var b2: [10]f64 = random_vector(10);
 
-  var z1: [256]f64 = undefined;
-  var a1: [256]f64 = undefined;
+  var z1: [20]f64 = undefined;
+  var a1: [20]f64 = undefined;
   var z2: [10]f64 = undefined;
   var a2: [10]f64 = undefined;
 
-  var right: f32 = 0;
-  var wrong: f32 = 0;
+  var right: f32 = 0.0;
   // Stochastic Gradient Descent
   // Only do one sample at a time
   // Note: It is far better to perform vectorization over the entire dataset
-  for (0..10) |epoch| {
+  for (0..50) |epoch| {
 
     std.debug.print("Epoch: {d}\n", .{epoch});
-    for (data_train.value, 0..) |data, iteration| {
+    std.debug.print("Acc: {d:.4}\n\n", .{100 * (right/60000.0)});
+    right = 0;
+
+    for (data_train.value) |data| {
       // Input layer (Data)
       const input: [784]f64 = data.image;
       const label: u8 = data.label;
@@ -190,92 +193,81 @@ pub fn main() !void {
       ////////////////////////////////////////////////
       // Forward Propogation /////////////////////////
       ////////////////////////////////////////////////
-      z1 = dot(f64, 256, 784, w1, input);
-      z1 = add(f64, 256, z1, b1);
-      a1 = ReLU(f64, 256, z1);
+      z1 = dot(f64, 20, 784, w1, input);
+      z1 = add(f64, 20, z1, b1);
+      a1 = ReLU(f64, 20, z1);
 
-      z2 = dot(f64, 10, 256, w2, a1);
+      z2 = dot(f64, 10, 20, w2, a1);
       z2 = add(f64, 10, z2, b2);
       a2 = softmax(f64, 10, z2);
+      // std.debug.print("{any}\n", .{a2});
 
-      const guess = vmax(f64, a2);
-      // Display Guess
-      // std.debug.print("Input: {d}    ", .{label});
-      // std.debug.print("Guess: {d}\n", .{guess});
-      if (label == guess) {
-        right += 1.0;
-      } else {
-        wrong += 1.0;
+      // Check if guess is right
+      if (vmax(f64, a2) == @as(usize, label)) {
+        right += 1;
       }
-
-      if (iteration % 100 == 0) {
-        std.debug.print("Accuracy: {d:.6}\n", .{right / (right + wrong)});
-      }
-
 
       ////////////////////////////////////////////////
       // Back Propogation ////////////////////////////
       ////////////////////////////////////////////////
       const y = one_hot(label);         // 10 x 1
 
-      var dz2: [10]f64 = undefined; // 10 x 1
-      for (0..10) |idx| {
-        dz2[idx] = a2[idx] - y[idx];
-      }
+      const dz2: [10]f64 = sub(f64, 10, a2, y); // 10 x 1
 
       // dw2 = dot(dz2, a1.transpose())
-      var dw2: [10][256]f64 = undefined;
+      var dw2: [10][20]f64 = .{.{0.0} ** 20} ** 10;
       for (dz2, 0..) |row, m| {       // 10 x 1
         for (a1, 0..) |col, n| {      // 256 x 1 transposed => 1 x 256
-          dw2[m][n] = (1/60000) * row * col;
+          dw2[m][n] = row * col;
         }
       }
-      const db2: f64 = (1/60000) * vsum(f64, 10, dz2);
+      const db2: f64 = vsum(f64, 10, dz2);
 
-      var dz1: [256]f64 = undefined;
+      var dz1: [20]f64 = .{0.0} ** 20;
       // dot( w2.T , dz2) * ReLU_deriv(z1);
-      for (0..256) |i| {
+      for (0..20) |i| {
         for (0..10) |j| {
           dz1[i] += w2[j][i] * dz2[j];
         }
         dz1[i] *= ReLU_deriv(f64, z1[i]);
       }
 
-      var dw1: [256][784]f64 = undefined;
+      // dw1 = dot(dZ1, input.T)
+      var dw1: [20][784]f64 = .{.{0.0} ** 784} ** 20;
       for (dz1, 0..) |row, m| {       // 256 x 1
         for (input, 0..) |col, n| {   // 784 x 1 transposed => 1 x 784
-          dw1[m][n] = (1/60000) * row * col;
+          dw1[m][n] = row * col;  //+ ((lambda) * w1[m][n]);
         }
       }
 
-      const db1: f64 = (1/60000) * vsum(f64, 256, dz1);
+      const db1: f64 = vsum(f64, 20, dz1);
 
       ////////////////////////////////////////////////
       // Update Network //////////////////////////////
       ////////////////////////////////////////////////
 
       // Update w1
-      for (w1, 0..) |row, m| {
-        for (row, 0..) |_, n| {
-          w1[m][n] -= alpha * dw1[m][n];
+      for (&w1, 0..) |*row, m| {
+        for (row, 0..) |*val, n| {
+          val.* -= alpha * dw1[m][n];
         }
       }
 
       // Update b1
-      for (b1, 0..) |_, m| {
-        b1[m] -= alpha * db1;
+      for (&b1) |*val| {
+        val.* -= alpha * db1;
       }
 
       // Update w2
-      for (w2, 0..) |row, m| {
-        for (row, 0..) |_, n| {
-          w2[m][n] -= alpha * dw2[m][n];
+      for (&w2, 0..) |*row, m| {
+        for (row, 0..) |*val, n| {
+          val.* -= alpha * dw2[m][n];
         }
       }
 
       // Update b2
-      for (b2, 0..) |_, m| {
-        b2[m] -= alpha * db2;
+      for (&b2) |*val| {
+        val.* -= alpha * db2;
       }
     }
   }
